e4cb28dbfca8d3d804440eeed96ec7fa7d9dd3c3
Don-001 <62480623+Don-001@users.noreply.github.com>
2022-09-07 19:15:09 +0300

    Update char_fun2.c

--- char_fun2.c
+++ char_fun2.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcmp - Compare Two String
- * @s1:String 1
- * @s2:String 2
+ * s1:String 1
+ * s2:String 2
  * Return: 0 If Identical Otherwise How Much Diffrent
  */
 int _strcmp(char *s1, char *s2)
